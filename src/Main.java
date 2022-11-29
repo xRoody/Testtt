2439482ccc886116041be3a524886269e6b6f566
xRoody <sasha_ermak_2002@mail.ru>
2022-11-29 10:50:47 +0300

    c

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello warld!");
+        System.out.println("Hello werld!");
+        System.out.println("Hello werld!");
+        System.out.println("Hello werld!");
+        System.out.println("Hello werld!");
+        System.out.println("Hello werld!");
+        System.out.println("Hello werld!");
     }
 }
